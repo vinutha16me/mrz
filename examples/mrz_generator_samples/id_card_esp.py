c39e157ff66552cb96b1dd565874cf0514efa768
vinutha16me <100337460+vinutha16me@users.noreply.github.com>
2024-11-14 13:14:38 +0530

    Update id_card_esp.py

--- examples/mrz_generator_samples/id_card_esp.py
+++ examples/mrz_generator_samples/id_card_esp.py
@@ -12,13 +12,13 @@ sys.path.append(realpath(join(dirname(__file__), pardir, pardir)))
 
 from mrz.generator.td1 import TD1CodeGenerator
 
-print(TD1CodeGenerator("ID",                 # Document type   Normally 'I' or 'ID' for id cards
-                       "ESP",                # Country         3 letters code or country name
-                       "BAA000589",          # Document number
+print(TD1CodeGenerator("C1",                 # Document type   Normally 'I' or 'ID' for id cards
+                       "USA",                # Country         3 letters code or country name
+                       "BAA230589",          # Document number
                        "800101",             # Birth date      YYMMDD
                        "F",                  # Genre           Male: 'M', Female: 'F' or Undefined
-                       "250101",             # Expiry date     YYMMDD
-                       "ESP",                # Nationality
-                       "ESPAÑOLA ESPAÑOLA",  # Surname         Special characters will be transliterated
-                       "CARMEN",             # Given name(s)   Special characters will be transliterated
+                       "260101",             # Expiry date     YYMMDD
+                       "MX",                # Nationality
+                       "das",  # Surname         Special characters will be transliterated
+                       "sun",             # Given name(s)   Special characters will be transliterated
                        "99999999R"))         # Optional data 1
